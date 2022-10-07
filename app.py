000da78378f59cccd1391979ab21e467e516f9e5
Frank Davies <fdavies93@gmail.com>
2022-10-08 00:21:30 +0800

    Sync is feature-complete.

--- app.py
+++ app.py
@@ -1,51 +1,50 @@
 from asyncio.subprocess import PIPE
-from cgi import test
-from io import FileIO
-import sys
-import subprocess
 import time
 import asyncio
 
 config_top = [{
     "local": "/home/frank/Documents/rclone-test/",
     "remote": "GDrive:rclone-test",
+    "poll_time": 20.0, # maximum time between syncs if no changes recorded (mostly downloads from cloud source)
+    "cooldown_time": 10.0 # minimum time between syncs on change
+},
+{
+    "local": "/home/frank/Documents/obsidian/",
+    "remote": "GDrive:Obsidian",
     "poll_time": 60.0, # maximum time between syncs if no changes recorded (mostly downloads from cloud source)
     "cooldown_time": 10.0 # minimum time between syncs on change
 }] 
 # list of settings objects
 
-async def setup_pipes(config):
-    source = config[0]
+async def setup_pipe(source):
     local = source["local"]
 
     pipe = await asyncio.create_subprocess_shell(f"inotifywait --recursive --monitor --event modify --event create --event delete --event move --format '%w%f' {local}", stdout=asyncio.subprocess.PIPE)
-    # pipe = subprocess.Popen(["inotifywait","--recursive","--monitor","--event", "modify", "--event", "create", "--event", "delete", "--event", "move", "--format", '%w%f', local], stdout=PIPE)
     return pipe
 
-async def main():
-    test_pipe = await setup_pipes(config_top)
+async def monitor(config):
+    test_pipe = await setup_pipe(config)
     last_sync = 0
-    config = config_top[0]
+    print(f"Watching {config['local']} for changes.")
     while True:
-        
         try:
-            ln = await test_pipe.stdout.readline()
-            
-            print(ln.decode()[:-1])
+            ln = await asyncio.wait_for( test_pipe.stdout.readline(), config["poll_time"] )
 
             if time.time() - last_sync > config["cooldown_time"]:
-                subprocess.run(["rclone", "sync", config["local"], config["remote"], "--verbose"])
-                last_sync = time.time()
+                print(f"Updating remote {config['remote']} from changed local directory {config['local']}.")
+                await asyncio.create_subprocess_shell(f"rclone sync {config['local']} {config['remote']} --quiet")
+                print(f"Update complete.")
         
         except asyncio.TimeoutError:
             # do bisync
-            pass
+            print(f"Synchronising local {config['local']} and remote {config['remote']}.")
+            await asyncio.create_subprocess_shell(f"rclone bisync {config['local']} {config['remote']} --quiet --resync")
+            print("Sync complete.")
+        
+        last_sync = time.time()
 
-        # ln : bytes = test_pipe.stdout.readline()
-        # print (ln.decode()[:-1])
-
-# subprocess.call() # call inotifywait to set up the sync
-# subprocess.call() # call rclone sync
+async def main():
+    await asyncio.gather(*[monitor(config) for config in config_top], return_exceptions=False)
 
 if __name__ == "__main__":
     asyncio.run(main())
