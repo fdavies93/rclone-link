207c987c833753fbd94157b50499f3b751594536
Frank Davies <fdavies93@gmail.com>
2022-10-07 23:31:41 +0800

    Made process to sync to remote on file changes / adds / deletes.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from asyncio.subprocess import PIPE
+from cgi import test
 from io import FileIO
 import sys
 import subprocess
 import time
+import asyncio
 
 config_top = [{
-    "local": "~/Documents/rclone-test",
+    "local": "/home/frank/Documents/rclone-test/",
     "remote": "GDrive:rclone-test",
     "poll_time": 60.0, # maximum time between syncs if no changes recorded (mostly downloads from cloud source)
     "cooldown_time": 10.0 # minimum time between syncs on change
@@ -15,27 +17,36 @@ config_top = [{
 def setup_pipes(config):
     source = config[0]
     local = source["local"]
-    pipe = subprocess.Popen(f"--quiet --recursive --monitor --event modify --format \"%w%f\" { local }", executable="inotifywait", stdout=PIPE)
+
+    
+    pipe = subprocess.Popen(["inotifywait","--recursive","--monitor","--event", "modify", "--event", "create", "--event", "delete", "--event", "move", "--format", '%w%f', local], stdout=PIPE)
     return pipe
-    # inotifywait --quiet --recursive --monitor --event modify --format "%w%f" /home/frank/Documents/obsidian
 
-def sync():
-    pass
-
-def main():
+async def main():
     test_pipe = setup_pipes(config_top)
+    last_sync = 0
+    config = config_top[0]
+    loop = asyncio.get_event_loop()
     while True:
-        ln : str = test_pipe.stdout.read()
-        if len(ln) > 0:
-            print(ln)
-    # setup pipe
-    # await changes
-    # if changes OR poll time is reached, 
-    # ignore changes that happen within cooldown period
-    pass
+        
+        try:
+            ln = test_pipe.stdout.readline()
+            
+            print(ln.decode()[:-1])
+
+            if time.time() - last_sync > config["cooldown_time"]:
+                subprocess.run(["rclone", "sync", config["local"], config["remote"], "--verbose"])
+                last_sync = time.time()
+        
+        except:
+            # do bisync
+            pass
+
+        # ln : bytes = test_pipe.stdout.readline()
+        # print (ln.decode()[:-1])
 
 # subprocess.call() # call inotifywait to set up the sync
 # subprocess.call() # call rclone sync
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
