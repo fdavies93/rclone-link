fb8baf4e5cf32c0517bc7b0cc2f00d6c8c1260ef
Frank Davies <fdavies93@gmail.com>
2022-10-10 22:31:05 +0800

    Changed sync method
    Sync method now uses direct pushes and pulls from / to remote based on updates to the local repositories.
    This means that it will work poorly if two people are editing the same file at the same time, like a version manager.
    However it should typically work better than mounts for cases where people aren't working on the same file at the same time.

--- app.py
+++ app.py
@@ -5,25 +5,55 @@ import datetime
 from enum import IntEnum
 
 sources = [{
-    "label": "Test",
-    "local": "/home/frank/Documents/rclone-test/",
-    "remote": "GDrive:rclone-test",
-    "poll_time": 60.0, # maximum time between syncs if no changes recorded (mostly downloads from cloud source)
+    "label": "Code Notes",
+    "local": "/home/frank/code/notes/",
+    "remote": "GDrive:code-notes",
+    "poll_time": 20.0, # maximum time between syncs if no changes recorded (mostly downloads from cloud source)
     "cooldown_time": 15.0 # minimum time between syncs on change
 },
 {
     "label": "Obsidian",
     "local": "/home/frank/Documents/obsidian/",
     "remote": "GDrive:Obsidian",
-    "poll_time": 60.0, # maximum time between syncs if no changes recorded (mostly downloads from cloud source)
+    "poll_time": 20.0, # maximum time between syncs if no changes recorded (mostly downloads from cloud source)
     "cooldown_time": 15.0 # minimum time between syncs on change
 }] 
 # list of settings objects
 
 class SYNC_MODE(IntEnum):
     NONE = 0
-    UPDATE = 1 # push local directory to remote, overwriting remote
+    PUSH = 1 # push local directory to remote, overwriting remote
     SYNC = 2 # try to merge local and remote, preserving changes to both
+    PULL = 3 # pull remote to local directory, overwriting local (this seems rare)
+
+def timestamp():
+    cur_time = time.time()
+    time_label = datetime.datetime.fromtimestamp(cur_time).strftime("%Y-%m-%d %H:%M:%S")
+    return time_label
+
+async def push(config):
+    label = config["label"]
+
+    print(f"[{label} | {timestamp()}] Pushing local {config['local']} to remote {config['remote']}.")
+    await asyncio.create_subprocess_shell(f"rclone sync {config['local']} {config['remote']} --quiet")
+
+    print(f"[{label} | {timestamp()}] Push complete.")
+
+async def pull(config):
+    label = config["label"]
+
+    print(f"[{label} | {timestamp()}] Pulling from remote {config['remote']} to local {config['local']}.")
+    await asyncio.create_subprocess_shell(f"rclone sync {config['remote']} {config['local']} --quiet")
+
+    print(f"[{label} | {timestamp()}] Pull complete.")
+
+async def sync(config):
+    label = config["label"]
+
+    print(f"[{label} | {timestamp()}] Synching local {config['local']} and remote {config['remote']}.")
+    await asyncio.create_subprocess_shell(f"rclone bisync {config['local']} {config['remote']} --quiet --resync")
+
+    print(f"[{label} | {timestamp()}] Sync complete.")
 
 async def setup_pipe(source):
     local = source["local"]
@@ -36,33 +66,32 @@ async def monitor(config):
     last_sync = 0
     label = config["label"]
     next_wait = config["poll_time"]
-    cur_time = time.time()
-    time_label = datetime.datetime.fromtimestamp(cur_time).strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{label} | {time_label}] Watching {config['local']} for changes.")
+
+    mode : SYNC_MODE = SYNC_MODE.PULL
+    
+    last_sync = time.time()
+
+    print(f"[{label} | {timestamp()}] Watching {config['local']} for changes.")
     while True:
         try:
-            ln = await asyncio.wait_for( test_pipe.stdout.readline(), next_wait )
-            cur_time = time.time()
-            time_label = datetime.datetime.fromtimestamp(cur_time).strftime("%Y-%m-%d %H:%M:%S")
-            if cur_time - last_sync > config["cooldown_time"]:
-                print(f"[{label} | {time_label}] Updating remote {config['remote']} from changed local directory {config['local']}.")
-                await asyncio.create_subprocess_shell(f"rclone sync {config['local']} {config['remote']} --quiet")
-                print(f"[{label} | {time_label}] Update complete.")
-                last_sync = time.time()
-            else:
-                next_wait = config["cooldown_time"] - (cur_time - last_sync) # wait for cooldown to expire then do a sync
-                print(f"[{label} | {time_label}] Waiting for {round(next_wait, 2)} seconds for cooldown to end.")
+            next_wait = config["poll_time"] - (time.time() - last_sync)
+
+            if next_wait > 0:
+                ln = await asyncio.wait_for( test_pipe.stdout.readline(), next_wait )
+                # if we never reach here (no local changes since last sync), it will pull from remote
+                # if we do reach here (local changes since last sync), it will push to remote
+                mode = SYNC_MODE.PUSH
         
         except asyncio.TimeoutError:
-            # do bisync
-            cur_time = time.time()
-            time_label = datetime.datetime.fromtimestamp(cur_time).strftime("%Y-%m-%d %H:%M:%S")
-            print(f"[{label} | {time_label}] Synchronising local {config['local']} and remote {config['remote']}.")
-            await asyncio.create_subprocess_shell(f"rclone bisync {config['local']} {config['remote']} --quiet --resync")
-            print(f"[{label} | {time_label}] Sync complete.")
-            next_wait = config["poll_time"]
+            # leave this block to make it a bit clearer what's happening
+            # / add possible trigger events later 
+            if mode == SYNC_MODE.PULL:
+                await pull(config)
+            elif mode == SYNC_MODE.PUSH:
+                await push(config)
+
+            mode = SYNC_MODE.PULL
             last_sync = time.time()
-        
 
 async def main():
     await asyncio.gather(*[monitor(config) for config in sources], return_exceptions=False)
